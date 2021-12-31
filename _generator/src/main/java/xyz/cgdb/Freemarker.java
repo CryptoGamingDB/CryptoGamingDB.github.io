c7adf895244eb86cfd0651fde845360392e1e621
peppy <PyromancerPeppy@protonmail.com>
2021-12-30 23:16:04 -0500

    move generator here

---
+++ _generator/src/main/java/xyz/cgdb/Freemarker.java
@@ -0,0 +1,28 @@
+package xyz.cgdb;
+
+import freemarker.template.Configuration;
+import freemarker.template.TemplateExceptionHandler;
+
+import java.io.FileWriter;
+import java.util.Map;
+
+public class Freemarker {
+    private final Configuration cfg;
+
+    public Freemarker() {
+        var cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setClassForTemplateLoading(this.getClass(), "/");
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+        cfg.setFallbackOnNullLoopVariable(false);
+        this.cfg = cfg;
+    }
+
+    void render(String template, String file, Map<Object, Object> data) throws Exception {
+        try (var writer = new FileWriter(file)) {
+            cfg.getTemplate(template).process(data, writer);
+        }
+    }
+}
